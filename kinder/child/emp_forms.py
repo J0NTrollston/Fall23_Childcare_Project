2bc22bc81e3c403553bc40a628716dbc30f189ea
nil gopani <nil2000gopani@gmail.com>
2023-11-06 18:32:42 -0500

    commit

--- kinder/child/emp_forms.py
+++ kinder/child/emp_forms.py
@@ -6,9 +6,11 @@ from .models import employee
 
 class EmployeeForm(forms.Form):
     Employee_First_Name  = forms.CharField()
-
-    
-    
+    Employee_Last_Name = forms.CharField()
+    Employee_Class = forms.CharField()
+    Employee_Address = forms.CharField()
+    Employee_Facility = forms.CharField()
+    Employee_Salary = forms.IntegerField()    
 
 
 ##    c_lname  = forms.CharField(max_length=30)
