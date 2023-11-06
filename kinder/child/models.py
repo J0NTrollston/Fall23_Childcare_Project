b316f5a8e2c3b28886af55e5528f7321f18c35c2
Emily Chamberlain <echamberlain2019@fau.edu>
2023-11-06 18:13:56 -0500

    New tables added

--- kinder/child/models.py
+++ kinder/child/models.py
@@ -1,36 +1,82 @@
 ##import uuid
 from django.db import models
-
-
-from django.db import models
 from django.db.models import Model
 
-# Create your models here.
 class child(models.Model):
-    ##id = models.UUIDField(primary_key = True,default = uuid.uuid4, editable = False)
-    ##child_id = models.AutoField(primary_key = True, editable = False)
+    Child_id = models.UUIDField(primary_key = True, editable = False)
     Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
     Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
-    #Child_DBO  = models.DateField()
-    Child_allergies  = models.CharField(max_length=200,null=False,blank=False)
-    Parent_FullName  = models.CharField(max_length=200,null=False,blank=False)
-    ##geeks_field = models.CharField(max_length = 200)
-##    c_lname  = models.CharField(max_length=30)
-##    c_class  = models.CharField(max_length=30)
-##    c_addr  = models.CharField(max_length=30)
-##    c_school  = models.CharField(max_length=30)
-##    c_enmail  = models.CharField(max_length=30)
+    Child_DOB  = models.DateField()
+    Child_Allergies  = models.CharField(max_length=200,null=False,blank=False)
+    Parent_First_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Phone = models.CharField(max_length=200,null=False,blank=False)
+    Child_Address = models.CharField(max_length=200,null=False,blank=False)
+    Consent_Form_Signed = models.CharField(max_length=4,null=False,blank=False)
 
+class parent(models.Model):
+    Parent_id = models.UUIDField(primary_key = True, editable = False)
+    Parent_First_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Phone = models.CharField(max_length=200,null=False,blank=False)
+    Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Child_DOB  = models.DateField()
+    Child_Allergies  = models.CharField(max_length=200,null=False,blank=False)
+    Child_Address = models.CharField(max_length=200,null=False,blank=False)
+    Sign_In_Date = models.DateField()
+    Sign_Out_Date = models.DateField()
+    Sign_In_Time =
+    Sign_Out_Time =
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
 
-
-# Create your models here.
 class employee(models.Model):
-    ##id = models.UUIDField(primary_key = True,default = uuid.uuid4, editable = False)
-    ##child_id = models.AutoField(primary_key = True, editable = False)
-    Employee_First_Name  = models.CharField(max_length=200,null=False,blank=False)
-    ##geeks_field = models.CharField(max_length = 200)
-##    c_lname  = models.CharField(max_length=30)
-##    c_class  = models.CharField(max_length=30)
-##    c_addr  = models.CharField(max_length=30)
-##    c_school  = models.CharField(max_length=30)
-##    c_enmail  = models.CharField(max_length=30)
+    employee_id = models.UUIDField(primary_key = True, editable = False)
+    Employee_First_Name = models.CharField(max_length=200,null=False,blank=False)
+    Employee_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Employee_DOB  = models.DateField()
+    Employee_Address = models.CharField(max_length=200,null=False,blank=False)
+    Classroom = models.
+    Hourly_Salary = models.
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
+
+class enrollments(models.Model):
+    Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Classroom = models.
+    Tuition_Fee = models.
+    Teacher_First_Name = models.CharField(max_length=200,null=False,blank=False)
+    Teacher_Last_Name = models.CharField(max_length=200,null=False,blank=False)
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
+
+class child_attendance(models.Model):
+    Sign_In_Date = models.DateField()
+    Sign_Out_Date = models.DateField()
+    Sign_In_Time = models.TimeField()
+    Sign_Out_Time = models.TimeField()
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
+    Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+
+class staff_attendance(models.Model):
+    Sign_In_Date = models.DateField()
+    Sign_Out_Date = models.DateField()
+    Sign_In_Time = models.TimeField()
+    Sign_Out_Time = models.TimeField()
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
+    Teacher_First_Name = models.CharField(max_length=200,null=False,blank=False)
+    Teacher_Last_Name = models.CharField(max_length=200,null=False,blank=False)
+    Hours_Worked = models.
+    Salary_Earned = models.
+
+class facility(models.Model):
+    Facility_Name = models.CharField(max_length=200,null=False,blank=False)
+    Facility_Address = models.CharField(max_length=200,null=False,blank=False)
+    Facility_Phone = models.
+    License_Number = models.IntegerField()
+    Admin_Name = models.CharField(max_length=200,null=False,blank=False)
+    Admin_Email = models.CharField(max_length=200,null=False,blank=False)
+    Admin_Phone = 
+
+
+
