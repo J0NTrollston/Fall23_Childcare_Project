a152cc70efe79ba8376176a7d711a64d9a5e6ede
nil gopani <nil2000gopani@gmail.com>
2023-11-06 18:36:46 -0500

    commit

--- kinder/child/forms.py
+++ kinder/child/forms.py
@@ -5,7 +5,7 @@ class ChilForm(forms.Form):
     Child_First_Name  = forms.CharField(label='Child First Name')
     Child_Last_Name  = forms.CharField(label='Child Last Name')
     Child_allergies  = forms.CharField(label='Child Allergies')
-    #Child_DBO  = forms.DateField()
+    Child_DBO = forms.DateField()
     Parent_FullName  = forms.CharField(label='Parent Full Name')
 
 ##    c_lname  = forms.CharField(max_length=30)
@@ -13,6 +13,7 @@ class ChilForm(forms.Form):
 ##    c_addr  = forms.CharField(max_length=30)
 ##    c_school  = forms.CharField(max_length=30)
 ##    c_enmail  = forms.CharField(max_length=30)
+##    Child_DBO  = forms.DateField()
 
 
 class SearchC (forms.Form):
