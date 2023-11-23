012b64a5b6dae2b312c971722ccac0b9d1466577
Emily Chamberlain <echamberlain2019@fau.edu>
2023-11-22 19:37:11 -0500

    Added emails and removed IDs

--- kinder/child/models.py
+++ kinder/child/models.py
@@ -5,23 +5,22 @@ from django.db.models import Model
 # Create your models here.
 
 class child(models.Model):
-    Child_id = models.UUIDField(primary_key = True, editable = False)
     Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
     Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
     Child_DoB  = models.DateField(max_length=9,null=False,blank=False)
     Child_allergies  = models.CharField(max_length=200,null=False,blank=False)
-
     Parent_First_Name = models.CharField(max_length=200,null=False,blank=False)
     Parent_Last_Name = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Email =  models.CharField(max_length=200,null=False,blank=False)
     Parent_Phone = models.CharField(max_length=10,null=False,blank=False)
     Parent_Address = models.CharField(max_length=200,null=False,blank=False)
     Consent_Box = models.CharField(max_length=200,null=False,blank=False)
 
 class parent(models.Model):
-    Parent_id = models.UUIDField(primary_key = True, editable = False)
     Parent_First_Name  = models.CharField(max_length=200,null=False,blank=False)
     Parent_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
     Parent_Phone = models.CharField(max_length=200,null=False,blank=False)
+    Parent_Email =  models.CharField(max_length=200,null=False,blank=False)
     Child_First_Name  = models.CharField(max_length=200,null=False,blank=False)
     Child_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
     Child_DOB  = models.DateField()
@@ -34,9 +33,9 @@ class parent(models.Model):
     Facility_Name = models.CharField(max_length=200,null=False,blank=False)
 
 class employee(models.Model):
-    employee_id = models.UUIDField(primary_key = True, editable = False)
     Employee_First_Name = models.CharField(max_length=200,null=False,blank=False)
     Employee_Last_Name  = models.CharField(max_length=200,null=False,blank=False)
+    Employee_Email =  models.CharField(max_length=200,null=False,blank=False)
     Employee_DOB  = models.DateField()
     Employee_Address = models.CharField(max_length=200,null=False,blank=False)
     Classroom = models.CharField(max_length=200)
