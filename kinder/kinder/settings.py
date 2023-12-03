8693fe327523e1b2495e2b05810b5d3cdc8ca247
Emily Chamberlain <echamberlain2019@fau.edu>
2023-12-02 19:57:34 -0500

    path for frontend directory

--- kinder/kinder/settings.py
+++ kinder/kinder/settings.py
@@ -114,16 +114,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
-##STATICFIELDS_DIRS=(os.path.join(BASE_DIR,'static'),)
-##<img src="{% static 'img/daycare_v1.png'%}" width=500 height = 300>
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+FRONTEND_DIR = "path" 
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]    
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
